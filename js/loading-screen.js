b76a296a84bb642d3a9596787ed504507366c68d
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2020-09-22 11:51:05 -0700

    loading screen transitions

--- js/loading-screen.js
+++ js/loading-screen.js
@@ -6,15 +6,24 @@
 // The basic check
 function done() {
     if(document.readyState === 'complete') {
-        document.getElementsByClassName("loading-screen")[0].classList.add("fade");
-        console.log("helee");
+
+        var loadingScreen = document.getElementsByClassName("loading-screen-active")[0];
+        if (loadingScreen !== null) {
+            loadingScreen.classList.add("fade");
+        }
     }
 }
 
+
 // Polling for the sake of my intern tests
 var interval = setInterval(function() {
+    setTimeout('', 1000);
+    console.log(document.readyState);
     if(document.readyState === 'complete') {
         clearInterval(interval);
         done();
+    }
+    else {
+        document.getElementsByClassName("loading-screen-wrapper")[0].classList.add("loading-screen-active");
     }    
 }, 100);
